e02e5be466107d144697352339a0c1635f301d36
Freshia-Njoki <Freshia-Njoki>
2023-02-17 11:39:52 -0800

    6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main -Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int i;
+	/* code goes here */
+	for (i = 48; i < 48; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
