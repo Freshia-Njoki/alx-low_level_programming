8f341efe85e862c8bd756c66a9b9838dedf1ee0f
Freshia-Njoki <Freshia-Njoki>
2023-03-02 16:03:41 -0800

    removed spaces

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,7 +9,7 @@ char *leet(char *s)
 {
 	int count = 0, i;
 	int low_letters[] = {97, 101, 11, 116, 108};
-	int upp_letters[] = {65, 69, 79, iimi:84, 76};
+	int upp_letters[] = {65, 69, 79, 84, 76};
 	int numbers[] = {52, 51, 48, 55, 49};
 
 	while (*(s + count) != '\0')
