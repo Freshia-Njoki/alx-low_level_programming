52154131adcdeb61788fe6bc46b4070543e60638
Freshia-Njoki <Freshia-Njoki>
2023-03-10 13:29:16 -0800

    argc argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,7 +6,7 @@
  * @argv: array that contain the program command line arguments
  * Return: 0 - success
  */
-int main(int argc, char *argv[] __attriute__((unuseed)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
 	printf("%d\n", argc - 1);
 	return (0);
