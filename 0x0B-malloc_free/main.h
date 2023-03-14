682b3bb3f3eef0b8369b1817a0c5396479297631
Freshia-Njoki <Freshia-Njoki>
2023-03-14 06:53:50 -0700

    2

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,8 +1,7 @@
 #ifndef _MAIN_H_
 #define _MAIN _H_
 
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
+char *create_array(unsigned int size, char c); char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
 int **alloc_grid(int width, int height);
 void free_grid(int **grid, int height);
